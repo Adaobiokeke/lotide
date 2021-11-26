dbf2c20165b906c6c9dc330723eb554e0b3a7a83
Adaobiokeke <adaobiotiji@yahoo.com>
2021-11-26 10:01:52 -0500

    Object shorthand challenge

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const head   = require('./head');
 const middle = require('./middle');
 const eqArrays   = require('./eqArrays')
 const assertEqual = require('./assertEqual')
-module.exports = {
-  head:   head,
-  middle: middle,
-  eqArrays: eqArrays,
-  assertEqual: assertEqual,
-};
+ 
+const allObjects = {
+    head,
+    middle,
+    eqArrays,
+    assertEqual
+}
+module.exports = allObjects;
